3ef3134f0f53ce086c1a1a1be20b944a25d255f7
redjumper <vmshare@gmail.com>
2023-07-08 00:44:59 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -15,4 +15,4 @@ then
 fi
 
 #Start Tailscale derp server
-/root/go/bin/derper --hostname $TAILSCALE_DERP_HOSTNAME --a $TAILSCALE_DERP_ADDR -certmode $TAILSCALE_DERP_CERTMODE -certdir /root/derper/$TAILSCALE_DERP_HOSTNAME --stun-port=$TAILSCALE_DERP_STUN_PORT --verify-clients=$TAILSCALE_DERP_VERIFY_CLIENTS
+/root/go/bin/derper --hostname=$TAILSCALE_DERP_HOSTNAME -a=$TAILSCALE_DERP_ADDR -http-port=-1 -certmode=$TAILSCALE_DERP_CERTMODE -certdir=/root/derper/$TAILSCALE_DERP_HOSTNAME --stun-port=$TAILSCALE_DERP_STUN_PORT --verify-clients=$TAILSCALE_DERP_VERIFY_CLIENTS
